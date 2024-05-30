5b84b65db63652cd6c0b3cded0157a5009ba878f
anjalikesherwani <122610979+anjalikesherwani@users.noreply.github.com>
2024-05-30 14:56:15 +0530

    PersonClass

---
+++ 28-05-2024 task-3/person.java
@@ -0,0 +1,16 @@
+// import java.util.io;
+import java.util.Scanner;
+public class person {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Your name is : " + name);
+
+        System.out.println("enter your age: ");
+        String age = sc.nextLine();
+        System.out.println("Your name is : " + age);
+    }
+}
